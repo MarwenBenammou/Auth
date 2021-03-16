165750fcedb43e9820f14222d73f20f636010b76
MarwenBenammou <76168205+MarwenBenammou@users.noreply.github.com>
2021-03-16 23:35:03 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+// Import Routes
+const authRoute = require('./Routes/auth');
+
+dotenv.config();
+
+//Coonect to DB
+mongoose.connect( process.env.DB_CONNECT,
+{useNewUrlParser: true, useUnifiedTopology: true},
+() => console.log('connected to DB')
+);
+
+//Middleware
+app.use(express.json());
+
+
+
+
+//Route Middlewares 
+app.use('/api/user', authRoute);
+
+
+app.listen(3000, () => console.log('Server Up and running'));
